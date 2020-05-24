4a4c52ec788374f7fc7db578523fdaf61c376315
juker1141 <50107434+juker1141@users.noreply.github.com>
2020-05-25 00:53:45 +0800

    update index.html

--- app/assets/js/all.js
+++ app/assets/js/all.js
@@ -1,3 +1,20 @@
-$(document).ready(() => {
-  console.log('Hello Bootstrap4');
+$('.navbar-toggler').click(function (e) {
+  e.preventDefault();
+  $('.nav-menu').addClass('active')
+})
+$('.goback_icon').click(function (e) {
+  e.preventDefault();
+  $('.nav-menu').removeClass('active')
+})
+$('.shopping_cartBtn').click(function (e) {
+  e.preventDefault();
+  $('.shopping-cart-page').addClass('active')
+})
+$('.shopping-cart-close-icon').click(function (e) {
+  e.preventDefault();
+  $('.shopping-cart-page').removeClass('active')
+})
+
+const mySwiper = new Swiper('.swiper-container', {
+  slidesPerView: 'auto',
 });
