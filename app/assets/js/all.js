7edb270400fc71982ac22ee28070eb9fabf36907
juker1141 <50107434+juker1141@users.noreply.github.com>
2020-05-29 00:16:59 +0800

    update index.html

--- app/assets/js/all.js
+++ app/assets/js/all.js
@@ -16,6 +16,39 @@ $('.shopping-cart-close-icon').click(function (e) {
   $('.shopping-cart-page').removeClass('active')
 })
 
+switch ($('title').text()) {
+  // case "Index":
+  //   $(".js-layout__navbar").addClass("fixed-top navbar-dark");
+  //   $('a[href="productList.html"]').addClass('custom__text');
+  //   break;
+  case "ProductList":
+    $('.product_logo_icon').addClass('active');
+    $('.menuBtn').addClass('active');
+    $('.shopping_cartBtn').addClass('active');
+    break;
+  case "Product":
+    $('a[href="productList.html"]').addClass('active');
+    break;
+  case "Information":
+    $(".section__subscribe").hide();
+    $(".js-layout__navbar__nav").hide();
+    $(".js-layout__navbar__footer").hide();
+    break;
+  case "Payment":
+    $(".section__subscribe").hide();
+    $(".js-layout__navbar__footer").hide();
+    break;
+  case "Well Done":
+    $(".section__subscribe").hide();
+    $(".js-layout__navbar__footer").hide();
+    break;
+}
+
 const mySwiper = new Swiper('.swiper-container', {
   slidesPerView: 'auto',
 });
+
+// $(".productlistBtn").click(function (e) {
+
+//   $(".product_logo_icon").addClass('active');
+// });
